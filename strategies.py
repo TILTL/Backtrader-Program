7a6588cbfefd77d684444bee636b447d3461d5fd
TILTL <131395335+TILTL@users.noreply.github.com>
2023-11-28 15:40:55 +0800

    Add files via upload

--- strategies.py
+++ strategies.py
@@ -75,7 +75,6 @@ class MAcrossover(bt.Strategy):
             #Otherwise if the 20 SMA is below the 50 SMA   
             elif self.fast_sma[0] < self.slow_sma[0] and self.fast_sma[-1] > self.slow_sma[-1]:
                 self.log(f'SELL CREATE {self.dataclose[0]:2f}')
-                # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
         else:
             # We are already in the market, look for a signal to CLOSE trades
@@ -83,29 +82,3 @@ class MAcrossover(bt.Strategy):
                 self.log(f'CLOSE CREATE {self.dataclose[0]:2f}')
                 self.order = self.close()
 
-class CPPI_Strategy(bt.Strategy):
-    params = (
-        ('floor', 0.8),  # 80% of initial portfolio value
-        ('multiplier', 3),  # CPPI multiplier
-    )
-
-    def __init__(self):
-        self.initial_portfolio_value = self.broker.getvalue()
-        self.floor_value = self.initial_portfolio_value * self.params.floor
-
-    def next(self):
-        total_value = self.broker.getvalue()
-        cushion = max(total_value - self.floor_value, 0)
-        risky_asset_allocation = self.p.multiplier * cushion
-        risk_free_allocation = total_value - risky_asset_allocation
-
-        # Assuming only one asset in the strategy for simplicity
-        risky_asset = self.datas[0]
-        current_position = self.getposition(risky_asset).size
-        current_value = current_position * risky_asset.close[0]
-
-        # Calculate the desired position size
-        target_position_size = risky_asset_allocation / risky_asset.close[0]
-
-        # Adjust the position
-        self.order_target_size(data=risky_asset, target=target_position_size)
